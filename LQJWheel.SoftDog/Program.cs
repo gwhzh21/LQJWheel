cdfd86560c90fd29fe9e16eabb5af3255787a95b
贺喆 <466879662@qq.com>
2025-08-25 17:29:03 +0800

    看门狗与管理界面。

---
+++ LQJWheel.SoftDog/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LQJWheel.SoftDog
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ LQJWheel.SoftDogManager/Form1.cs
@@ -0,0 +1,10 @@
+namespace LQJWheel.SoftDogManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
